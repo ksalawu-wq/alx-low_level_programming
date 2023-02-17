79d3295a0ac4d95205126a9e64e8929058477294
ksalawu <sakabiru11@gmail.com>
2023-02-16 16:20:41 -0800

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+/**
+ * main - print size of data types
+ * Description: use printf and sizeof
+ * Return: 0
+ */
+int main()
+{
+	printf("%zu, %zu, %zu, %zu, %zu", sizeof(int),sizeof(char),sizeof(long int),sizeof(long long int)sizeof(float));
+	return (0);
+}
