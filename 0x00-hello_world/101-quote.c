62ecfe6e04965100804c0ab7b974af913dabd7f9
ksalawu <sakabiru11@gmail.com>
2023-02-16 19:19:08 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - print quote
+ *
+ * Return: 1
+ */ 
 
 int main(void) {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return EXIT_SUCCESS;
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
+
