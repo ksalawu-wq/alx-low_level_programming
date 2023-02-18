916f0e774c829bedd0b8b75523981dba2874adc3
ksalawu <sakabiru11@gmail.com>
2023-02-18 14:50:13 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,9 +22,9 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
-	else if (l < 6 && l != 0)
+	else (l < 6 && l != 0)
 	{
-		printf("Lsat digit of %d is %d and is less than 6 and not 0\n", n, l);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
 }
