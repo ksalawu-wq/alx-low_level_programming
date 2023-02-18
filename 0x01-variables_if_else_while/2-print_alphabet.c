59cb3250b0b03750f3f5d87765e42a772e41aaa6
ksalawu <sakabiru11@gmail.com>
2023-02-18 15:09:03 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: prints alphabets in lowercase
+ * Return; returns 0
+ */
+
+int main(void)
+{
+	int al;
+
+	for (ch = 'a', ch <= 'z', ch++)
+	{
+		puchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
