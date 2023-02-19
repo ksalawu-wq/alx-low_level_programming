c58263d9fd2af038a058c275e2da09d9dc7c60af
ksalawu <sakabiru11@gmail.com>
2023-02-19 13:12:11 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * main - print quote
- *
- * Return: 1
- */
+#include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (error)
+ */
 int main(void)
 {
-fwrite(stderr, "and that piece of art is useful\"
-		- Dora Korpar, 2015-10-19\n");
-return (1);
-}
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+    /* Use write() to print the message to the standard error */
+    write(2, message, sizeof(message) - 1);
+
+    /* Return 1 to indicate an error occurred */
+    return (1);
+}
