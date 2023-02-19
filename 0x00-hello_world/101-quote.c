3bdd1cb2d621b38d9288e70aad305beb6b21d8fa
ksalawu <sakabiru11@gmail.com>
2023-02-19 13:08:13 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,12 @@
  * main - print quote
  *
  * Return: 1
- */ 
+ */
 
-int main(void) {
-write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+int main(void)
+{
+fwrite(stderr, "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n");
 return (1);
 }
 
