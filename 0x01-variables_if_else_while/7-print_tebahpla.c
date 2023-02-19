b4d459594c7a1190858826235c7f60cdecd92411
ksalawu <sakabiru11@gmail.com>
2023-02-19 04:20:47 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: print alphabets in reverse orders
+ * Return: returns 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a', ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
