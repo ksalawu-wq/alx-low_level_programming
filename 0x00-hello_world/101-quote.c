a23231a23a0deaa8da93eabc0f5598d0cc5d6aae
ksalawu <sakabiru11@gmail.com>
2023-02-19 13:20:58 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    char message[] =
+        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
     /* Use write() to print the message to the standard error */
     write(2, message, sizeof(message) - 1);
