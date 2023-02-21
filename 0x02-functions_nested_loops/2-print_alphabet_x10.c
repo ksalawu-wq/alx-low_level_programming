108d4a57ac450e7ec10eaa302dd8b6a258844e2f
ksalawu <sakabiru11@gmail.com>
2023-02-21 08:28:59 -0800

    alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,14 @@ void print_alphabet_x10(void)
 {
 	char i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 10;)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
 		_putchar(j);
 		}
+
+		i++;
 	}
 	_putchar('\n');
 }
