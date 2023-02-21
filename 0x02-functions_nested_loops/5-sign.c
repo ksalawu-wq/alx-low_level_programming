a070d30637640337e9d91a9ab1c9fb69f76c7942
ksalawu <sakabiru11@gmail.com>
2023-02-21 14:57:51 -0800

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,25 +9,25 @@
 
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
 		_putchar('-');
 		_putchar(',');
 		_putchar(' ');
-		return (-1);
+		return (1);
 	}
-	else if (n > 0)
+	else if (n == 0)
 	{
 		_putchar('+');
 		_putchar(',');
 		_putchar(' ');
-		return (1);
+		return (0);
 	}
 	else
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		return (0);
+		return (-1);
 	}
 }
