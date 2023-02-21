03bc389004e9ea8d4e3212bd96c8fe30b6233107
ksalawu <sakabiru11@gmail.com>
2023-02-21 04:43:42 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include <unistd.h>
 #include "main.h"
 /**
  * main - Entry point
- * Description: To print the program _putchar
- * Return: returns ) (success)
+ * Description: prints 'Holberton \n'
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	char str[] = "_putchar"
-	int i = 0;
+char str[] = "main\n";
+int i = 0;
 
-	while (str[i] != '\0')
+while (str[i] != '\0')
 	{
-		char a = str[i];
+	char c = str[i];
 
-		_putchar(a);
-		i++;
+	_putchar(c);
+	i++;
 	}
-	return (0);
+return (0);
 }
