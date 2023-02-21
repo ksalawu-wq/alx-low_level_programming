52a235bc57d926c95b2d92626127314e605080cd
ksalawu <sakabiru11@gmail.com>
2023-02-21 05:53:18 -0800

    print 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,6 @@
  * print_alphabet_x10: prints the alphabet in lowercase 10 times
  * Return: Always 0.
  */
-int main(void)
-{
-print_alphabet_x10();
-return (0);
-}
 void print_alphabet_x10(void)
 {
 	char i, j;
