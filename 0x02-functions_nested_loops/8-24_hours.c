408813b101c01e0b1e6a6b2212325c31fafbd9e8
ksalawu <sakabiru11@gmail.com>
2023-02-22 05:09:07 -0800

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer() - prints 24hrs
+ * @i:  parameters for hour
+ * @j: parameters for minutes
+ * Return: always 0
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j ++)
+		{
+			_puchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
