779129461e06ecd0fb482b40dc4efe36c744bf97
ksalawu <sakabiru11@gmail.com>
2023-02-22 07:22:50 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,9 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -23,7 +25,9 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+                                printf(", ");
 		}
 		printf("\n");
 	}
@@ -32,6 +36,8 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d, ", i);
+			if (i != 98)
+                                printf(", ");
 		}
 		printf("\n");
 	}
