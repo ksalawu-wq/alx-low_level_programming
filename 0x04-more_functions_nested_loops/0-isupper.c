7267d4e0891f534771a8b8f063b535124598ef2b
ksalawu <sakabiru11@gmail.com>
2023-02-23 02:41:50 -0800

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - checks uppercase
+ * @c: parameter to be checked
+ * Return: 1 if uppercase
+ */
+
+int _isupper(int c)
+{
+	while (c = 'A')
+	{
+		_putchar(c);
+	}
+	return (1);
+}
