8079c2d53d0f47796bfbe75209c2e952b32fca5b
ksalawu <sakabiru11@gmail.com>
2023-02-23 03:14:10 -0800

    0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - function that print numbers
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
