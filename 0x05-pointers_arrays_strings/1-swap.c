9805b55609418f522886c32c932cae1348d6b653
ksalawu <sakabiru11@gmail.com>
2023-02-27 04:11:33 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* swap_int - swaps variables
+* @a: variable to swap
+* @b: variable to swap
+*/
+
+void swap_int(int *a, int *b)
+{
+	int n = *a;
+
+	*a = *b;
+	*b = *n;
+
+}
