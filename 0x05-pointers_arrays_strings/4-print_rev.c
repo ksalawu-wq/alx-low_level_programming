4530b1a18c80983e15d768b8f8f8d5e15813cc23
ksalawu <sakabiru11@gmail.com>
2023-02-27 04:36:02 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int a = 0;
+	int b;
+
+	while (*s != '\0')
+	{
+	a++;
+	s++;
+	}
+	s--;
+	for (b = a; b > 0; b--);
+	{
+	_putchar(*s);
+	s--;
+	}
+_putchar('\n');
+}
