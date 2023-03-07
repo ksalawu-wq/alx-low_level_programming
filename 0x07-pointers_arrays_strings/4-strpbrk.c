81ce240797283f8068a665b99b0da7b1602af455
ksalawu <sakabiru11@gmail.com>
2023-03-06 23:13:39 -0800

    update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,13 +12,13 @@ char *_strpbrk(char *s, char *accept)
 	int i;
 
 	while (*s)
-{
-	for (i = 0; accept[i] != '\0'; i++)
+	{
+	for (i = 0; accept[i]; i++)
 	{
 	if (*s == accept[i])
 	return (s);
 	}
-s++;
-}
-return('\0');
+	s++;
+	}
+	return('\0');
 }
