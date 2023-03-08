54f27ba967b3b9ddd81b624ce870491c59c77f15
ksalawu <sakabiru11@gmail.com>
2023-03-08 01:04:41 -0800

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,17 @@
 #include "main.h"
 
-
+/**
+* factorial - factorial of integers
+* @n: input
+* Return: n
+*/
 
 int factorial(int n)
 {
-
+	if (n < 0)
+	return (-1);
+	else if (n > 0)
+	return (n * factorial(n - 1));
+	else
+	return (1);
 }
