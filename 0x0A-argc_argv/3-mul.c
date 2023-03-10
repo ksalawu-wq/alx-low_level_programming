6561d47eae1a1a70bd5dfb384acb39e2f3794b2a
ksalawu <sakabiru11@gmail.com>
2023-03-10 03:19:49 -0800

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,2 +1,28 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* @argc: argument count
+* @argv: argument vector
+* Return: 1 if more than three-
+* - argument else 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int mul;
+
+	if (argc != 3)
+	{
+	printf("more than two arguments");
+	return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
+	printf("%d\n", mul);
+return (0);
+}
