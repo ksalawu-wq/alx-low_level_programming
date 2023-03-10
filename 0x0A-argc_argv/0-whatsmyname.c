56e9b63b902a93dfd37551c7768fbe55e1bad917
ksalawu <sakabiru11@gmail.com>
2023-03-10 01:27:38 -0800

    my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,23 @@
+#include "main.h"
 #include <stdio.h>
+
+/**
+* main - Entry point
+* @argc: integer
+* @argv: character pointer
+* Return: 0
+*/
 
 int main(int argc, char *argv[])
 {
-(void)argc;
-	printf("%s\n", argv[0]);
+int a = 0;
 
+	while (argv[0][a] != '\0')
+{
+	putchar(argv[0][a]);
+	a++;
+}
+putchar('\n');
+(void)argc;
 return (0);
 }
