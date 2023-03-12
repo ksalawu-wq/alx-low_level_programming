cad5855f9d77a323f1c6e025a770f8b842384446
ksalawu-wq <sakabiru11@gmail.com>
2023-03-12 04:44:47 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,9 +3,9 @@
 
 /**
 * main - Entry point
-* argc: argument count
-* argv: argument vector
-* return: 0
+* @argc: argument count
+* @argv: argument vector
+* Return: 0
 */
 
 int main(int argc, char **argv[])
