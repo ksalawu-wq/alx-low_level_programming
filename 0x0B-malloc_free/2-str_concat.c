c0dacffccd7216686110de8ae88a8135e559adde
ksalawu-wq <sakabiru11@gmail.com>
2023-03-13 09:54:45 -0700

    str-concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* str_concat - concatenates strings
+* @s1: input
+* @s2: input
+* Return: new_str
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	size_t len1, len2, len3;
+	char *new_str;
+
+	if (s1 == NULL || s2 == NULL)
+	return (NULL);
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	len3 = len1 + len2;
+
+	new_str = malloc((len3 + 1) * sizeof(char));
+	if (new_str == NULL)
+	return (NULL);
+
+	strcpy(new_str, s1);
+	strcat(new_str, s2);
+
+return (new_str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
+char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
